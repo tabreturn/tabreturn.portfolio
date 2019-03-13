af2255e671296a4442288919b70a1213c2ddc0bd
tabreturn <github@tabreturn.com>
2019-03-14 12:53:06 +1300

    - add update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+rm -r about.html css favicon.png id.html illustration.html images print.html research.html web.html;
+cd tabreturn.portfolio;
+git pull;
+~/.gem/ruby/2.5.0/bin/jekyll build;
+cd ..;
+cp -r tabreturn.portfolio/_site/about.html tabreturn.portfolio/_site/css tabreturn.portfolio/_site/favicon.png tabreturn.portfolio/_site/id.html tabreturn.portfolio/_site/illustration.html tabreturn.portfolio/_site/images tabreturn.portfolio/_site/print.html tabreturn.portfolio/_site/research.html tabreturn.portfolio/_site/web.html ./
