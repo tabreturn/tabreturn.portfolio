4b10b93beb98d186dc9fd51cfa603f628e719a6e
tabreturn <github@tabreturn.com>
2019-10-14 22:16:16 +1300

    - rename cv to resume

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
-rm -r about.html cv.html css favicon.png interactive.html images lecturing.html not-so-interactive.html;
+rm -r about.html css favicon.png interactive.html images lecturing.html not-so-interactive.html resume.html;
 cd tabreturn.portfolio;
 git pull;
 ~/.gem/ruby/2.5.0/bin/jekyll build;
 cd ..;
 cp -r tabreturn.portfolio/_site/about.html ./;
-cp -r tabreturn.portfolio/_site/cv.html ./;
 cp -r tabreturn.portfolio/_site/css ./;
 cp -r tabreturn.portfolio/_site/favicon.png ./;
 cp -r tabreturn.portfolio/_site/interactive.html ./;
 cp -r tabreturn.portfolio/_site/images ./;
 cp -r tabreturn.portfolio/_site/lecturing.html ./;
 cp -r tabreturn.portfolio/_site/not-so-interactive.html ./;
+cp -r tabreturn.portfolio/_site/resume.html ./;
