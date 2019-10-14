7af658fc22695bdd02c65eea06062459086ea7d9
tabreturn <github@tabreturn.com>
2019-10-14 18:34:02 +1300

    - add semi-colons to bash script

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ cd tabreturn.portfolio;
 git pull;
 ~/.gem/ruby/2.5.0/bin/jekyll build;
 cd ..;
-cp -r tabreturn.portfolio/_site/about.html ./
-cp -r tabreturn.portfolio/_site/cv.html ./
-cp -r tabreturn.portfolio/_site/css ./
-cp -r tabreturn.portfolio/_site/favicon.png ./
-cp -r tabreturn.portfolio/_site/interactive.html ./
-cp -r tabreturn.portfolio/_site/images ./
-cp -r tabreturn.portfolio/_site/lecturing.html ./
-cp -r tabreturn.portfolio/_site/not-so-interactive.html ./
+cp -r tabreturn.portfolio/_site/about.html ./;
+cp -r tabreturn.portfolio/_site/cv.html ./;
+cp -r tabreturn.portfolio/_site/css ./;
+cp -r tabreturn.portfolio/_site/favicon.png ./;
+cp -r tabreturn.portfolio/_site/interactive.html ./;
+cp -r tabreturn.portfolio/_site/images ./;
+cp -r tabreturn.portfolio/_site/lecturing.html ./;
+cp -r tabreturn.portfolio/_site/not-so-interactive.html ./;
